19097ee73ca6c0c419bb0acd3b4ebf601b65011d
sabbir1257 <masabbir1257@gmail.com>
2025-03-04 11:00:07 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AddCoffee from './components/AddCoffee.jsx'
+import UpdateCoffee from './components/UpdateCoffee.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/addCoffee',
+    element: <AddCoffee/>
+  },
+  {
+    path: 'updateCoffee',
+    element: <UpdateCoffee/>
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
